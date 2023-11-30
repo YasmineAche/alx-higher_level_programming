912a6810537339d860ca5860da96abb2a5681e23
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2023-11-30 13:55:09 +0100

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     import hidden_4
 
     liste = dir(hidden_4)
+    names = set()
     for l in liste:
         if not l.startswith("__"):
-            print(l)
+            names.add(l)
+    for name in sorted(names):
+        print(name)
