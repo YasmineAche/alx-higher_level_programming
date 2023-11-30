1a9c8518c9009a3a21db68bbb5e416949de13639
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2023-11-30 14:52:13 +0100

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    liste = dir(hidden_4)
-    for l in liste:
-        if l[:2] != "__":
-            print(l)
+    liste_names = dir(hidden_4)
+    for name in liste_names:
+        if name[:2] != "__":
+            print(name)
