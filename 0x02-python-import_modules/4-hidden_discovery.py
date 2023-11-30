cc003bdc116b4950f71caf42a1b68986826b78ed
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2023-11-30 14:49:10 +0100

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    liste = dir(hidden_4)
-    for l in liste:
-        if l[:2] != "__":
-            # if not l.startswith("__"):
-            print(l)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
