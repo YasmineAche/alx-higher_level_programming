bf8c1f8a07149a9d72a9595fe67353204773e449
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2023-12-06 15:44:46 +0100

    Task 12

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -3,6 +3,9 @@
 """
 roman_to_int = __import__("12-roman_to_int").roman_to_int
 
+roman_number = "MMMCMXCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
 roman_number = "X"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
