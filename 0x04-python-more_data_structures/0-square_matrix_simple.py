3f4137c0cd3bbb85fd2646cee1ae637b4e049e0a
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2023-12-06 12:02:09 +0100

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
+    new_matrix = []
+    for row in matrix:
+        new_row = map(lambda x: x * x, row)
+        new_matrix.append(list(new_row))
     return new_matrix
